82a36d1c47a5d4e45e5c0be61d2b135d73752320
imassenda <salmaimassenda2023@gmail.com>
2025-12-13 09:38:28 +0100

    init: setup microservices infrastructure (config, eureka, gateway)

---
+++ backend/services/discovry-server/src/test/java/com/example/discovryserver/DiscovryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discovryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscovryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
